374cf749ebd0661a7b06477ea88ef08f5c87839b
xx <1329629708@qq.com>
2022-11-14 20:38:15 +0800

    完成了路由与页面的功能

---
+++ src/router/modules/setting.js
@@ -0,0 +1,17 @@
+import Layout from '@/layout'
+// 导出员工的路由规则
+export default {
+  path: '/setting', // 路由地址
+  name: 'setting',
+  component: Layout,
+  children: [{
+    // 二级路由的path什么都不用写的时候 此时它表示二级路由的默认路由
+    path: '', // 这里不用写  什么都不用写表示 /employees 不但有布局 layout => 员工主页
+    component: () => import('@/views/setting'),
+    // 路由元信息 其实就是一个存储数据的地方 可以放任何内容
+    meta: {
+      title: '公司设置', // 因为左侧导航读取了这里的title属性
+      icon: 'setting'
+    }
+  }]
+}
